ead4b6ae357fbb7486a0f1ff45e71d3b31b24343
KrishnenduDG <krishnendu.kdg@gmail.com>
2024-04-11 16:38:53 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,jsonify
+from eventlet import wsgi
+import eventlet
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/",methods = ["GET"])
+    def get_route():
+        return jsonify(status = "success", message= "Hello")
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+
+    app.run(host="0.0.0.0",port = "4000",debug = True)
+    wsgi.server(eventlet.listen(("0.0.0.0", 4000)), app, debug=False)
